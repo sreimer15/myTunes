729351f8763b21acaeec1f0e573f6f4fbc2cd43d
Sean Reimer <sreimer15@gmail.com>
2015-12-09 19:01:41 -0600

    Passes Specs for App model and AppView

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,17 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    // this.on('add', )
+    // this.on('remove', )
   }
+  // enqueue: function() {
+
+  // },
+  // dequeue: function() {
+
+  // },
+  // playFirst: function() {
+
+  // }
 
 });
